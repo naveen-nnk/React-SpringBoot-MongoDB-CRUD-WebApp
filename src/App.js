3db96979441b8dbb924461ebda5fd70f6561de87
Naveen Kumar <naveenkumar.nnk21@gmail.com>
2024-10-19 00:10:13 +0530

    Working ReactWebApp First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch,withRouter} from 'react-router-dom';
+import ListUserComponent from './components/ListUserComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateUserComponent from './components/CreateUserComponent';
+import ViewUserComponent from './components/ViewUserComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Switch> 
+                          <Route exact path = "/" component = {withRouter(ListUserComponent)}></Route>
+                          <Route path = "/users" component = {withRouter(ListUserComponent)}></Route>
+                          <Route path = "/add-user/:id" component = {withRouter(CreateUserComponent)}></Route>
+                          <Route path = "/view-user/:id" component = {withRouter(ViewUserComponent)}></Route>
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
     </div>
+    
   );
 }
 
---
+++ src/components/ViewUserComponent.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import UserService from '../services/UserService'
+
+class ViewUserComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            user: {}
+        }
+    }
+
+    componentDidMount(){
+        UserService.getUserById(this.state.id).then( res => {
+            this.setState({user: res.data});
+        })
+    }
+
+    cancel(){
+        this.props.history.push('/users');
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                <div className = "card col-md-6 offset-md-3">
+                    <h3 className = "text-center"> View User Details</h3>
+                    <div className = "card-body">
+                        <div className = "row">
+                            <label> User First Name: </label>
+                            <div> { this.state.user.firstName }</div>
+                        </div>
+                        <div className = "row">
+                            <label> User Last Name: </label>
+                            <div> { this.state.user.lastName }</div>
+                        </div>
+                        <div className = "row">
+                            <label> User Email ID: </label>
+                            <div> { this.state.user.emailId }</div>
+                        </div>
+                    </div>
+                    <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Back</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ViewUserComponent
